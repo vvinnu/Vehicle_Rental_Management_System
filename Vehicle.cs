1074b9ae3c8357c4236161a86c6c9716e9a8c851
Vineeth Kanoor <Vineeth Kanoor@VINNI>
2024-03-27 20:06:56 -0400

    Added Vehicle Class Code

--- Vehicle.cs
+++ Vehicle.cs
@@ -1,1 +1,24 @@
-﻿
+﻿public class Vehicle
+{
+    // Properties of the Vehicle class
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public int Year { get; set; }
+    public double RentalPrice { get; set; }
+
+    // Constructor for Vehicle class
+    public Vehicle(string model, string manufacturer, int year, double rentalPrice)
+    {
+        Model = model;
+        Manufacturer = manufacturer;
+        Year = year;
+        RentalPrice = rentalPrice;
+    }
+
+    // Method to display the Vehicle Details in main program
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"Model: {Model}\nManufacturer: {Manufacturer}\nYear: {Year}\nRental Price: {RentalPrice}");
+    }
+}
+
